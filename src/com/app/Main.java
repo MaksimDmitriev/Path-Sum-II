f93277c77018ebab6cb421265a13d829cdce00fd
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2018-10-07 22:24:08 +0300

    Add f2

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -5,6 +5,22 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        f2();
+    }
+
+    private static void f2() {
+        Solution.TreeNode root = new Solution.TreeNode(1);
+        root.right = new Solution.TreeNode(2);
+        root.right.right = new Solution.TreeNode(3);
+        root.right.right.right = new Solution.TreeNode(4);
+        root.right.right.right.right = new Solution.TreeNode(5);
+
+        Solution solution = new Solution();
+        List<List<Integer>> res = solution.pathSum(root, 15);
+        System.out.println(res);
+    }
+
+    private static void f1() {
         Solution.TreeNode root = new Solution.TreeNode(5);
         root.left = new Solution.TreeNode(4);
         root.right = new Solution.TreeNode(8);
